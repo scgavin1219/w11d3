48d4f3d67be727087c55d1e63e6d14a661584755
matthew <matthewgoodbar@gmail.com>
2022-12-14 14:42:21 -0800

    Finish phase 2 for greenhouse

--- greenhouse/src/components/Greenhouse/LightSwitch.js
+++ greenhouse/src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,22 @@
 import './LightSwitch.css';
+import { useTheme } from '../../context/ThemeContext';
 
 function LightSwitch() {
+
+  const { themeName, setThemeName } = useTheme();
+
+  const handleOn = (e) => {
+    setThemeName("day");
+  }
+
+  const handleOff = (e) => {
+    setThemeName("night");
+  }
+  
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className={`light-switch ${themeName}`}>
+      <div className="on" onClick={handleOn}>DAY</div>
+      <div className="off" onClick={handleOff}>NIGHT</div>
     </div>
   );
 }
