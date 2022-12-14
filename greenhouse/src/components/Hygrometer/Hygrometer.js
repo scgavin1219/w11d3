fb04f63f631ba34815bd56f5d65453f8697e6387
matthew <matthewgoodbar@gmail.com>
2022-12-14 15:37:42 -0800

    Finish bonus phases 1 and 2

--- greenhouse/src/components/Hygrometer/Hygrometer.js
+++ greenhouse/src/components/Hygrometer/Hygrometer.js
@@ -1,16 +1,36 @@
 import ReactSlider from "react-slider";
 import "./Hygrometer.css";
 import { useClimate } from '../../context/ClimateContext'
+import { useState, useEffect } from "react";
 
 function Hygrometer() {
+
   const { humidity, setHumidity } = useClimate()
+
+  const [desiredHumidity, setDesiredHumidity] = useState(humidity);
+
+  const humidAdjust = () => {
+    if (desiredHumidity > humidity) {
+      setHumidity(oldHumid => oldHumid+1);
+    } else if (desiredHumidity < humidity){
+      setHumidity(oldHumid => oldHumid-1)
+    }
+  }
+
+  useEffect(() => {
+    const humidTimeout = setTimeout(humidAdjust, 1000);
+    return () => {
+      clearTimeout(humidTimeout);
+    }
+  }, [humidity, desiredHumidity])
+  
   return (
     <section>
       <h2>Hygrometer</h2>
       <div className="actual-humid">Actual Humidity: {humidity}%</div>
       <ReactSlider
-        value={humidity}
-        onAfterChange={(val) => {setHumidity(val)}}
+        value={desiredHumidity}
+        onAfterChange={(val) => {setDesiredHumidity(val)}}
         className="hygrometer-slider"
         thumbClassName="hygrometer-thumb"
         trackClassName="hygrometer-track"
--- greenhouse/src/components/Thermometer/Thermometer.js
+++ greenhouse/src/components/Thermometer/Thermometer.js
@@ -1,17 +1,36 @@
 import ReactSlider from "react-slider";
 import './Thermometer.css';
 import { useClimate } from '../../context/ClimateContext'
+import { useEffect, useState } from "react";
 
 function Thermometer() {
 
   const { temperature, setTemperature } = useClimate()
+
+  const [desiredTemp, setDesiredTemp] = useState(temperature);
+
+  const tempAdjust = () => {
+    if (desiredTemp > temperature) {
+      setTemperature(oldTemp => oldTemp+1);
+    } else if (desiredTemp < temperature) {
+      setTemperature(oldTemp => oldTemp-1);
+    }
+  }
+
+  useEffect(() => {
+    const adjustTimeout = setTimeout(tempAdjust, 1000);
+    return () => {
+      clearTimeout(adjustTimeout);
+    }
+  }, [desiredTemp, temperature])
+  
   return (
     <section>
       <h2>Thermometer</h2>
       <div className="actual-temp">Actual Temperature: {temperature}°F</div>
       <ReactSlider
-        value={temperature}
-        onAfterChange={(val) => {setTemperature(val)}}
+        value={desiredTemp}
+        onAfterChange={(val) => {setDesiredTemp(val)}}
         className="thermometer-slider"
         thumbClassName="thermometer-thumb"
         trackClassName="thermometer-track"
