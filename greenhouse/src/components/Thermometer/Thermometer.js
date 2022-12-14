b010cf98aced89657ee8e6f144a47a5f84d08c29
Dylan Gavin <dyl.gavin@gmail.com>
2022-12-14 15:06:08 -0800

    phase 4 complete

--- greenhouse/src/components/Thermometer/Thermometer.js
+++ greenhouse/src/components/Thermometer/Thermometer.js
@@ -1,14 +1,17 @@
 import ReactSlider from "react-slider";
 import './Thermometer.css';
+import { useClimate } from '../../context/ClimateContext'
 
 function Thermometer() {
+
+  const { temperature, setTemperature } = useClimate()
   return (
     <section>
       <h2>Thermometer</h2>
       <div className="actual-temp">Actual Temperature: {"x"}°F</div>
       <ReactSlider
-        value={40}
-        onAfterChange={(val) => {}}
+        value={temperature}
+        onAfterChange={(val) => {setTemperature(val)}}
         className="thermometer-slider"
         thumbClassName="thermometer-thumb"
         trackClassName="thermometer-track"
