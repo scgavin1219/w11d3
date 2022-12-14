20ee2cc803096d1091f80a6f508718edd792482b
Dylan Gavin <dyl.gavin@gmail.com>
2022-12-14 14:32:23 -0800

    phase 1 of greenhouse

--- practice-for-week-14-react-context-greenhouse-long-practice/src/components/Greenhouse/Greenhouse.js
+++ practice-for-week-14-react-context-greenhouse-long-practice/src/components/Greenhouse/Greenhouse.js
@@ -1,16 +1,31 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
-
+import { useState, useEffect } from 'react';
+import { useTheme } from '../../context/ThemeContext';
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
 
+  const { themeName } = useTheme()
+
+  const [image, setImage] = useState(dayImage)
+
+  useEffect(() => { 
+    if (themeName === 'day') {
+      setImage(dayImage)
+    } else {
+      setImage(nightImage)
+    }
+  }, [themeName])
+  
+
   return (
+  
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
+            src={image}
             alt='greenhouse' 
       />
       <LightSwitch />
